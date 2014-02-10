068a6adf2ed801f18958bc239b9b750918560116
Peter <peterhsaxton@gmail.com>
2014-02-10 09:15:41 +0000

    Correct implementation of public folder and css

--- router.rb
+++ router.rb
@@ -4,10 +4,6 @@ require 'haml'
 require './lib/statusController'
 
 class Router < Sinatra::Base
-	set :static, true
-	# set :public_dir, 'public'
-	# set :app_file, __FILE__
-	# set :root, File.dirname(__FILE__)
 
 	get '/' do
 		haml :index
@@ -17,10 +13,4 @@ class Router < Sinatra::Base
 		StatusController.call(env)
 	end
 
-	get "/public/stylesheets/reset.css" do
-	  content_type 'text/css'
-	  # sass :"/stylesheets/sass/reset.sass"
-	  "body { background: green; }"
-	  # send_file('reset.css')
-	end
 end
